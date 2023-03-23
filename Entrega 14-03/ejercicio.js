8cfb1a511acd77f0931345771ffd501e3f959f0e
DiegoAlonso92 <diego.alonso.uy@gmail.com>
2023-03-23 10:28:03 -0300

    desafío clase 3

--- Entrega 14-03/ejercicio.js
+++ Entrega 14-03/ejercicio.js
@@ -1,30 +1,4 @@
 
-// class ProductManager {
-
-//     constructor(){
-//     this.products = [];
-// }
-
-//     getProducts() {
-//         return this.products;
-//     }
-
-//     addProduct(product) {
-//     this.products.push(product);
-
-//     this.idAuto = this.idAuto + 1;
-//     };
-
-//     getProductById(productId) {
-// const product = this.product.find((product) => product.id === productId);
-
-// if (!product)
-// {
-//     throw Error('El producto no existe.');
-// }
-
-// };
-// }
 
 class ProductManager {
     constructor() {
@@ -36,12 +10,12 @@ class ProductManager {
       const { title, description, price, thumbnail, code, stock } = product;
   
       if (!title || !description || !price || !thumbnail || !code || !stock) {
-        console.error('Todos los datos son requeridos.');
+        throw Error ('Todos los datos son requeridos.');
         return;
       }
   
       if (this.products.some(p => p.code === code)) {
-        console.error('El código de ese producto ya existe.');
+        throw Error ('El código de ese producto ya existe.');
         return;
       }
   
@@ -65,16 +39,15 @@ class ProductManager {
     getProductById(id) {
       const product = this.products.find(p => p.id === id);
       if (!product) {
-        console.error('Not found');
+        console.error('Id de producto no encontrado.');
       }
       return product;
     }
   }
   
-  // Example usage
   const manager = new ProductManager();
-  manager.addProduct({ title: 'Product 1', description: 'Desc 1', price: 10, thumbnail: 'img1.jpg', code: 'P001', stock: 5 });
-  manager.addProduct({ title: 'Product 2', description: 'Desc 2', price: 20, thumbnail: 'img2.jpg', code: 'P002', stock: 10 });
+  manager.addProduct({ title: 'Harina 0000.', description: 'Harina de trigo 0000.', price: 80, thumbnail: 'imgHarina0000.jpg', code: 'P001', stock: 500 });
+  manager.addProduct({ title: 'Aceite de oliva.', description: 'Aceite de oliva extra virgen.', price: 350, thumbnail: 'imgAceiteDeOliva.jpg', code: 'P002', stock: 100 });
   console.log(manager.getProducts());
   console.log(manager.getProductById(1));
-  console.log(manager.getProductById(3));
+  console.log(manager.getProductById(2));
